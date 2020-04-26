81b6a3accc3e47a8fcefb1744d39e9e3bdc310c2
Ali <ali.bulut@datassist.com.tr>
2020-04-26 14:12:37 +0300

    users services edit

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -14,6 +14,7 @@ import {
   Typography
 } from '@material-ui/core';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+const axios = require('axios');
 
 const schema = {
   firstName: {
@@ -186,8 +187,24 @@ const SignUp = props => {
   };
 
   const handleSignUp = event => {
+    console.log(formState.values)
     event.preventDefault();
-    history.push('/');
+    axios.defaults.baseURL = 'http://localhost:3000';
+    axios.post('/singup/add/',formState.values)
+    axios({
+      method: 'Post',
+      url: '/singup/add/',
+      body: {
+        firstName:formState.values.firstName ,
+        lastName:formState.values.lastName ,
+        password:formState.values.password ,
+        email:formState.values.email ,
+      }
+    })
+      .then(res=> console.log(res.data))
+      .catch(err=>console.log(err.response.data));
+
+    //history.push('/');
   };
 
   const hasError = field =>
